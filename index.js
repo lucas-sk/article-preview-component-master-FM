0725c4f7d1a1bd052cff75db08ec96289afcf86b
lusk <lucash.lusk@gmail.com>
2022-07-23 11:02:02 -0300

    corrigindo estilos e refatorando o js

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
 const socialMedia = document.querySelector('.social-media');
-const shareBtn = document.querySelector('.share-btn');
+const socialMediaBtn = document.querySelector('.social-media button');
 const perfil = document.querySelector('.perfil')
-
-function createButton(className){
-  let button = document.createElement('button');
-  button.className = className + ' share-btn-active'
-  button.setAttribute('onClick', 'showSocialMedia()')
-  let img = document.createElement('img');
-  img.setAttribute('src', './images/icon-share.svg')
-  img.setAttribute('alt', 'icon share')
-  button.appendChild(img)
-
-  return button
-}
-
-if (window.matchMedia("(max-width: 500px)").matches) { 
-  socialMedia.appendChild(createButton('share-btn'))
-}
+const perfilBtn = document.querySelector('.perfil button');
 
 function showSocialMedia(){
-  if (window.matchMedia("(max-width: 500px)").matches) { 
+  if (window.matchMedia("(min-width: 501px)").matches) {
     if (socialMedia.classList.contains('hidden')){
       socialMedia.classList.remove('hidden');
-      perfil.classList.add('hidden');
-      shareBtn.classList.add('share-btn-active');
+      perfilBtn.classList.add('share-btn-active');
     }
     else{
       socialMedia.classList.add('hidden');
-      perfil.classList.remove('hidden');
-      shareBtn.classList.remove('share-btn-active');
-    }
+      perfilBtn.classList.remove('share-btn-active');
+    } 
   } 
   else {
     if (socialMedia.classList.contains('hidden')){
       socialMedia.classList.remove('hidden');
-      shareBtn.classList.add('share-btn-active');
+      perfil.classList.add('hidden');
+      socialMediaBtn.classList.remove('hidden');
+      socialMediaBtn.classList.add('share-btn-active');
     }
     else{
       socialMedia.classList.add('hidden');
-      shareBtn.classList.remove('share-btn-active');
+      perfil.classList.remove('hidden');
     }
+    
   }
 }
